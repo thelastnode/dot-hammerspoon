3242a89beb2a99a30d9f673a59ebfe7355e9a1cc
Ankit Shankar <ashankar@palantir.com>
2024-06-04 22:30:17 -0400

    Refactor launch mode

--- init.lua
+++ init.lua
@@ -1,52 +1,22 @@
--- Disabling once generated to make sure startup is quick.
+-- EmmyLua generates types for Hammerspoon development.
+-- It adds to startup time, so it's commented out here after running once. Uncomment to generate types:
 -- hs.loadSpoon('EmmyLua')
 
 hs.hotkey.bind({ 'cmd', 'shift', 'ctrl' }, 'r', function()
     hs.reload()
 end)
 
--- launch mode
+-- Globals:
+-- LaunchModal is shared between several modules, initializing here:
 LaunchModal = hs.hotkey.modal.new({ 'cmd', 'ctrl' }, 'a')
 LaunchModal:bind({}, 'escape', function() LaunchModal:exit() end)
 
--- app mappings
-local appMap = {
-    { key = 'v', app = '^Code$' },
-    { key = 's', app = 'Slack' },
-    { key = 'n', app = 'Obsidian' },
-    { key = 't', app = 'Microsoft Teams' },
-    { key = 'i', app = 'Messages' },
-    { key = 'f', app = 'Figma' },
-    { key = 'p', app = 'Spotify' },
-    { key = 'w', app = 'WhatsApp' },
-    { key = 'j', app = 'IntelliJ' },
-    { key = 'b', app = 'Blender' },
-    { key = 'e', app = 'Microsoft Edge' },
-    { key = 'x', app = 'Excel' },
-    { key = 'm', app = 'Messenger' },
-    { key = 'q', app = 'Xcode' },
-}
-local function focusApplication(pattern)
-    local app = hs.application.find(pattern)
-    if app ~= nil then
-        app:activate()
-    end
-end
-for _, mapping in ipairs(appMap) do
-    LaunchModal:bind({}, mapping['key'], function()
-        focusApplication(mapping['app'])
-        LaunchModal:exit()
-    end)
-end
-
--- Hammerspoon console
-LaunchModal:bind({}, '=', function() hs:openConsole(); LaunchModal:exit() end)
-
--- other modules
+-- Modules:
 require('cameralight')
 require('chrome')
 require('figma')
+require('launch')
 require('screenrecording')
-require('timetracker')
+require('timetracker.timetracker')
 
 hs.alert.show('Hammerspoon loaded 🔨')
---
+++ launch.lua
@@ -0,0 +1,32 @@
+-- app mappings
+local appMap = {
+    { key = 'v', app = '^Code$' },
+    { key = 's', app = 'Slack' },
+    { key = 'n', app = 'Obsidian' },
+    { key = 't', app = 'Microsoft Teams' },
+    { key = 'i', app = 'Messages' },
+    { key = 'f', app = 'Figma' },
+    { key = 'p', app = 'Spotify' },
+    { key = 'w', app = 'WhatsApp' },
+    { key = 'j', app = 'IntelliJ' },
+    { key = 'b', app = 'Blender' },
+    { key = 'e', app = 'Microsoft Edge' },
+    { key = 'x', app = 'Excel' },
+    { key = 'm', app = 'Messenger' },
+    { key = 'q', app = 'Xcode' },
+}
+local function focusApplication(pattern)
+    local app = hs.application.find(pattern)
+    if app ~= nil then
+        app:activate()
+    end
+end
+for _, mapping in ipairs(appMap) do
+    LaunchModal:bind({}, mapping['key'], function()
+        focusApplication(mapping['app'])
+        LaunchModal:exit()
+    end)
+end
+
+-- Hammerspoon console
+LaunchModal:bind({}, '=', function() hs:openConsole(); LaunchModal:exit() end)
